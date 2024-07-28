d4deef7d195e8a443d5cb6e44d5a5f5e472eab33
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2024-07-29 01:32:56 +0530

    signin and signup routes done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,34 +1,80 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { decode, sign, verify } from "hono/jwt";
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
 
-app.post('/api/v1/user/signup', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post("/api/v1/user/signup", async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-app.post('/api/v1/user/signin', (c) => {
-  return c.text('Hello Hono!')
-})
+  // Todo: add zod and hashed password
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username: body.username,
+        password: body.password,
+        name: body.name,
+      },
+    });
+    const jwt = await sign(
+      {
+        id: user.id,
+      },
+      c.env.JWT_SECRET,
+    );
+    return c.json({ jwt });
+  } catch (e) {
+    c.status(403);
+    return c.json({ error: "error while signing up" });
+  }
+});
 
-app.post('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post("/api/v1/user/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-app.put('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where: {
+      username: body.username,
+      password: body.password,
+    },
+  });
+  if (!user) {
+    c.status(403);
+    return c.json({ error: "user not found" });
+  }
+  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+  return c.json({ jwt });
+});
 
-app.get('/api/v1/blog/:id', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post("/api/v1/blog", (c) => {
+  return c.text("Hello Hono!");
+});
 
-app.get('/api/v1/blog/bulk', (c) => {
-  return c.text('Hello Hono!')
-})
+app.put("/api/v1/blog", (c) => {
+  return c.text("Hello Hono!");
+});
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-export default app
+app.get("/api/v1/blog/:id", (c) => {
+  return c.text("Hello Hono!");
+});
 
+app.get("/api/v1/blog/bulk", (c) => {
+  return c.text("Hello Hono!");
+});
 
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+export default app;
