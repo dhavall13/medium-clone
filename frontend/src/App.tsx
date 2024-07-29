768f2870635e65129b10039946df7fa466763572
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2024-07-29 09:56:23 +0530

    frontend project init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Hello, Tailwind CSS with Vite!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
