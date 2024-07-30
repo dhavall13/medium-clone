a7a19c7b7ac5fd322a4fde5f496ea9294821d7db
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2024-07-30 22:45:59 +0530

    added blog frontend

---
+++ frontend/src/pages/Blogs.tsx
@@ -0,0 +1,45 @@
+import { BlogCard } from "../components/BlogCard.tsx";
+import { Appbar } from "../components/Appbar.tsx";
+import { useBlogs } from "../hooks";
+import { BlogSkeleton } from "../components/BlogSkelton.tsx";
+
+export const Blogs = () => {
+  const { loading, blogs } = useBlogs();
+
+  if (loading) {
+    return (
+      <div>
+        <Appbar />
+        <div className="flex justify-center">
+          <div>
+            <BlogSkeleton />
+            <BlogSkeleton />
+            <BlogSkeleton />
+            <BlogSkeleton />
+            <BlogSkeleton />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <Appbar />
+
+      <div className="flex justify-center">
+        <div>
+          {blogs.map((blog) => (
+            <BlogCard
+              id={blog.id}
+              authorName={blog.author.name || "Anonymous"}
+              title={blog.title}
+              content={blog.content}
+              publishedDate={"2nd Feb 2024"}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
